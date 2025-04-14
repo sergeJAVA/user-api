b66d5657d84b478535dd78aad5cdf100e0ca3ad5
Sergey Kurochkin <serega.curo4kin2014@gmail.com>
2025-04-14 14:11:52 +0300

    Now when the life of a token expires, we generate a new one

--- src/main/java/com/example/user_api/config/security/JwtPerRequestFilter.java
+++ src/main/java/com/example/user_api/config/security/JwtPerRequestFilter.java
@@ -7,6 +7,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -29,10 +31,34 @@ public class JwtPerRequestFilter extends OncePerRequestFilter {
         String token = TokenUtil.parseToken(request);
 
         if (Optional.ofNullable(token).isPresent()) {
+            if (jwtService.isTokenExpired(token)) {
+                token = jwtService.refreshJwtToken(jwtService.parseExpiredTokenData(token));
+                setJwtCookie(response, token, 604800);
+            }
             TokenAuthentication authentication = new TokenAuthentication(jwtService.parseToken(token));
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    private void setJwtCookie(HttpServletResponse response, String token, int maxAge) {
+        ResponseCookie cookie = ResponseCookie.from("token", token)
+                .httpOnly(true) // Рекомендуется для безопасности
+                .secure(true) // Отправлять только по HTTPS
+                .path("/") // Или другой путь, в зависимости от требований
+                .maxAge(maxAge)
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+    }
+
+    private void clearJwtCookie(HttpServletResponse response, String name) {
+        ResponseCookie cookie = ResponseCookie.from(name, "")
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(0) // Установка maxAge в 0 удаляет Cookie
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
     }
 }
--- src/main/java/com/example/user_api/service/security/JwtService.java
+++ src/main/java/com/example/user_api/service/security/JwtService.java
@@ -2,16 +2,19 @@ package com.example.user_api.service.security;
 
 import com.example.user_api.model.security.TokenData;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -60,5 +63,76 @@ public class JwtService {
             throw new IllegalArgumentException("Error! Wrong argument passed!");
         }
 
+    }
+
+    public TokenData parseExpiredTokenData(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                    .clockSkewSeconds(60)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+
+            return TokenData.builder()
+                    .token(token)
+                    .username(claims.getSubject())
+                    .authorities(getAuthoritiesFromClaims(claims))
+                    .id(claims.get("userId", Long.class))
+                    .build();
+        } catch (ExpiredJwtException e) {
+            Claims claims = e.getClaims();
+            return TokenData.builder()
+                    .token(token)
+                    .username(claims.getSubject())
+                    .authorities(getAuthoritiesFromClaims(claims))
+                    .id(claims.get("userId", Long.class))
+                    .build();
+        } catch (JwtException | IllegalArgumentException e) {
+            log.error("Error parsing expired token: {}", e.getMessage());
+            throw new IllegalArgumentException("Unable to parse expired token");
+        }
+    }
+
+    private List<SimpleGrantedAuthority> getAuthoritiesFromClaims(Claims claims) {
+        Object rolesObj = claims.get("roles");
+        if (rolesObj instanceof Collection<?> roles) {
+            return roles.stream()
+                    .filter(String.class::isInstance)
+                    .map(String.class::cast)
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
+    }
+
+    public boolean isTokenExpired(String token) {
+        try {
+            return getAllClaimsFromToken(token).getExpiration().before(new Date());
+        } catch (ExpiredJwtException e) {
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            log.error("Error while parsing token for expiration check: {}", e.getMessage());
+            return true;
+        }
+    }
+
+    public String refreshJwtToken(TokenData tokenData) {
+        Map<String, Object> claims = new HashMap<>();
+
+        claims.put("username", tokenData.getUsername());
+        claims.put("roles", tokenData.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toSet()));
+        claims.put("userId", tokenData.getId());
+
+        log.info("Jwt has been refreshed!");
+        return Jwts.builder()
+                .claims(claims)
+                .subject(tokenData.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1800000))
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                .compact();
     }
 }
